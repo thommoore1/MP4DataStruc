93feb000917b9f2d1cc7ad9ad01306daf3804ab9
dtsivkovski <dtsivkovs@gmail.com>
2024-04-12 11:49:19 -0700

    window beginnings

--- Window.h
+++ Window.h
@@ -7,6 +7,21 @@ using namespace std;
 
 class Window {
 
+    public:
+        Window();
+        ~Window();
+
+        bool isEmpty();
+        void approachWindow(Student s);
+        Student* student;
+
+    private:
+        // time tracking variables
+        int longestIdletime;
+        int idleOver5;
+        int totalIdle;
+
+
 };
 
 
